755df01c1c90d4fbccf848848159046173bddd65
Jebbss <jebsumer@yahoo.com>
2017-05-12 08:42:58 -0500

    Update Q22.py

--- Q22.py
+++ Q22.py
@@ -10,42 +10,39 @@ is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 497
 What is the total of all the name scores in the file?
 '''
 def main():
-    count = 0
     tmpstr = ""
-    mylist = []
-    scorez = []
+    NameList = []
     f = open('names.txt', 'r')
+    NameList = ParseMe(f, NameList, tmpstr)
+    NameList.sort()
+    NamePosition = 1
+    NameScoreSum = 0
+    while NameList:
+        tmpstr = NameList.pop(0)
+        NameScoreSum += (NamePosition * ScoreMe(tmpstr))
+        NamePosition += 1
+    f.close()
+    print NameScoreSum
+
+def ParseMe(NameFile, NameList, tmpstr):
     while True:
-        c = f.read(1)
+        c = NameFile.read(1)
         if not c:
-            mylist.append(tmpstr)
+            NameList.append(tmpstr)
             tmpstr = ''
-            count += 1
             break
         if c == ',':
-            mylist.append(tmpstr)
+            NameList.append(tmpstr)
             tmpstr = ''
-            count += 1
         elif c != '"':
             tmpstr += c
-    mylist.sort()
-    pos = 1
-    sumz = 0
-    while mylist:
-        tmpstr = mylist.pop(0)
-        sumz += (pos * scoreMe(tmpstr))
-        if tmpstr == 'COLIN':
-            print "pos " , pos
-            print "score " , scoreMe(tmpstr)
-        pos += 1
-    f.close()
-    print sumz
+    return NameList
 
-def scoreMe(name):
-    scorez = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5, 'F': 6, 'G' : 7, 'H': 8, 'I': 9, 'J' : 10,
+def ScoreMe(name):
+    LetterScores = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5, 'F': 6, 'G' : 7, 'H': 8, 'I': 9, 'J' : 10,
               'K': 11, 'L': 12, 'M' : 13, 'N': 14, 'O' : 15, 'P' : 16, 'Q': 17, 'R': 18, 'S' : 19,
               'T' : 20, 'U' : 21, 'V' : 22, 'W': 23, 'X' : 24, 'Y' : 25, 'Z' : 26}
     score = 0
     for x in range (len(name)):
-        score += scorez[name[x]]
+        score += LetterScores[name[x]]
     return score
