6a392c4d8c8c271b743450f553eb36fd4e15a89d
Jeb Sumeracki <jebsumer@yahoo.com>
2017-05-20 11:36:01 -0500

    Add files via upload

---
+++ sieveprimes.py
@@ -0,0 +1,11 @@
+#generates primes to n, see Sieve of Eratosthenes for algorithm 
+def primes_to(n):
+    size = n//2 #floored quotient of n/2
+    sieve = [1]*size
+    limit = int(n**0.5)
+    for i in range(1,limit):
+        if sieve[i]:
+            val = 2*i+1
+            tmp = ((size-1) - i)//val 
+            sieve[i+val::val] = [0]*tmp
+    return [2] + [i*2+1 for i, v in enumerate(sieve) if v and i>0]
